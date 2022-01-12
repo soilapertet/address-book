d281707b488e3b97269cca0f6ec69d6c457975ed
Soila Pertet <nicolepertet@gmail.com>
2022-01-12 04:19:26 +0300

    Add a prototype method to display the full name

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,9 @@ function Contact(first,last) {
   this.firstName = first;
   this.lastName = last;
 }
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
 // User-interface logic
 $(document).ready(function(){
   $("form#new-contact").submit(function(event){
@@ -14,7 +17,7 @@ $(document).ready(function(){
     // Saving the collected input in an object
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");  
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");  
 
     // Clears the form after hitting the button
     $("input#new-first-name").val("");
